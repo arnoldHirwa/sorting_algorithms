7a6e867b9021af778893af4f25855a5580671d28
Hirwa Arnold <arnold.hirw@gmail.com>
2023-12-19 14:24:56 +0200

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,14 +1,41 @@
 #include "sort.h"
 
 /**
- * selection_sort - sort array using selection_sort Algorithm
- *
- * @array: array to sort
- * @size: size of the array
- *
- * Return: None
+* selection_sort - sort array using selection_sort Algorithm
+*
+* @array: array to sort
+* @size: size of the array
+*
+* Return: None
 */
 void selection_sort(int *array, size_t size)
 {
+	size_t minPosition, swapPosition;
+	int minValue;
+	size_t i = 0, j;
 
+	if (array == NULL || size < 2)
+		return;
+
+	minPosition = 0;
+	swapPosition = 0;
+	for ( ; i < size - 1; i++)
+	{
+		minValue = array[minPosition];
+		for (j = minPosition + 1; j < size; j++)
+		{
+			if (array[j] < minValue)
+			{
+				minValue = array[j];
+				swapPosition = j;
+			}
+		}
+		if (array[swapPosition] != array[minPosition])
+		{
+			array[swapPosition] = array[minPosition];
+			array[minPosition] = minValue;
+			print_array(array, size);
+		}
+		minPosition++;
+	}
 }
